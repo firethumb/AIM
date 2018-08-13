fd065a99e498821855c668ed2d62386dd0d76176
corneliohydie <corneliohydie@gmail.com>
2018-08-13 22:25:21 +0800

    updates

---
+++ AIM-LoopBackEnd/server/datasources.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = {
+  yourdbname: {
+    host: '/cloudsql/projectaim-206706:asia-southeast1:mysqldb1',
+    database: 'AIM', 
+    password: '@iMaPp#0607',
+    user: 'aim-app',
+    name: 'yourdbname',
+    connector: 'mysql',
+  },
+};
