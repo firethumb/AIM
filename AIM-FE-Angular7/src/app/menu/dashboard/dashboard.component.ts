d00495180853a90fdfb7cc313546d0da8452b072
Marcelino Basmayor <basmayom@ter.teradyne.com>
2018-12-05 15:20:23 +0800

    Addedd chartjs dashboard

--- AIM-FE-Angular7/src/app/menu/dashboard/dashboard.component.ts
+++ AIM-FE-Angular7/src/app/menu/dashboard/dashboard.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Chart} from 'chart.js';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-
+export class DashboardComponent implements OnInit{
+  canvas:any;
+  ctx:any;
   constructor() { }
 
   ngOnInit() {
+    this.drawChart('doughnut')
   }
 
+  drawChart(chartType){
+    this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+    let myChart = new Chart(this.ctx, {
+      type: chartType,
+      data: {
+          labels: ["New", "In Progress", "On Hold"],
+          datasets: [{
+              label: '# of Votes',
+              data: [1,2,3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+        responsive: false,
+        display:true
+      }
+    });
+  }
+  
 }
