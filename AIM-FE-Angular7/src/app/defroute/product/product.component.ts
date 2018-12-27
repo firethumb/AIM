5974ff32806960e6755bc353a9e06c7de9dbe062
LFLaquio <louie.laquio@gmail.com>
2018-12-27 15:58:08 +0800

    added content to /default/product

--- AIM-FE-Angular7/src/app/defroute/product/product.component.ts
+++ AIM-FE-Angular7/src/app/defroute/product/product.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import 'popper.js';
+import 'bootstrap';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
 
